315e0457766694bcfb16cd5b420cd1e8c2ed4d60
Priyadharshini Manikandan <priyamanikandan2206@gmail.com>
2024-05-09 10:45:18 -0400

    Create BET,LIKE,REGEXP.sql

---
+++ BET,LIKE,REGEXP.sql
@@ -0,0 +1,34 @@
+SELECT * FROM store.customers
+
+-- BETWEEN operator
+-- WHERE birth_date BETWEEN '1990-01-01' AND '2000-01-01'
+
+-- LIKE operator 
+-- % any number of characters
+-- - single character
+-- WHERE last_name LIKE '%y'
+-- WHERE last_name LIKE '%b%'
+-- WHERE last_name LIKE '%d'
+-- WHERE last_name LIKE '_____Y'
+
+-- WHERE address LIKE '%TRAIL%' OR address LIKE '%AVENUE%'
+-- WHERE phone LIKE '%9'
+-- WHERE phone NOT LIKE '%9'
+
+-- REGEXP operator
+-- WHERE first_name REGEXP 'Elka|ambur'
+-- WHERE last_name REGEXP 'ey$|on$'
+-- WHERE last_name REGEXP '^MY|SE'
+-- WHERE last_name REGEXP 'B[RU]'
+
+-- LIMIT clause
+-- ORDER BY points DESC
+-- LIMIT 3
+
+
+
+
+
+
+
+
