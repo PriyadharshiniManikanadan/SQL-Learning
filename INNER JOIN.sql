608cb1f5671f300398796587dcf999cfb9e4be40
Priyadharshini Manikandan <priyamanikandan2206@gmail.com>
2024-05-20 22:53:07 -0400

    Create INNER JOIN.sql

---
+++ INNER JOIN.sql
@@ -0,0 +1,8 @@
+-- INNER JOIN 
+-- SELECT *
+-- FROM order_items OI
+-- JOIN products P on OI.product_id = P.product_id
+
+-- SELECT order_id, OI.product_id, quantity, OI.unit_price
+-- FROM order_items OI
+-- JOIN products P on OI.product_id = P.product_id
