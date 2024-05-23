d63078534398a737637a14412ea46841a8a674f7
Priyadharshini Manikandan <priyamanikandan2206@gmail.com>
2024-05-23 08:59:55 -0400

    Create INSERTING MULTIPLE ROWS.sql

---
+++ INSERTING MULTIPLE ROWS.sql
@@ -0,0 +1,10 @@
+INSERT INTO products (name,quantity_in_stock,unit_price)
+VALUES ('Priya',
+		 100,
+         10.65),
+	   ('Manikandan',
+		 55,
+         5.28),
+		('Utsav',
+		 79,
+         9.14)
