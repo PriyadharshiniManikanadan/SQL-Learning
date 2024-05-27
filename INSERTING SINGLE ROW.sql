54dbea7c5d95505e3e885c92c559c6d5aa7ddbca
Priyadharshini Manikandan <priyamanikandan2206@gmail.com>
2024-05-27 17:19:50 -0400

    Create INSERTING SINGLE ROW.sql

---
+++ INSERTING SINGLE ROW.sql
@@ -0,0 +1,8 @@
+INSERT INTO customers (first_name,last_name,birth_date,phone,address,city,state)
+VALUES ('Priya',
+		'Manikandan',
+        '1992-08-09',
+        NULL,
+        'Corbin Ave',
+        'Jersey City',
+        'NJ')
