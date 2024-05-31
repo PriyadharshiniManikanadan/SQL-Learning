afc98a42fbc81a444b3350619242ff7032d01b13
PriyadharshiniManikanadan <145161399+PriyadharshiniManikanadan@users.noreply.github.com>
2024-05-31 08:49:23 -0400

    Update OUTER JOIN.sql

--- OUTER JOIN.sql
+++ OUTER JOIN.sql
@@ -1,20 +1,20 @@
 -- OUTER JOIN 
 
--- LEFT JOIN 
+ LEFT JOIN 
  SELECT 
      p.product_id,
      p.name,
      oi.quantity
- -- FROM products p
- -- LEFT JOIN order_items oi
- --        ON p.product_id = oi.product_id
+  FROM products p
+  LEFT JOIN order_items oi
+        ON p.product_id = oi.product_id
  
 
--- RIGHT JOIN
+ RIGHT JOIN
 
- -- FROM products p
- -- RIGHT JOIN order_items oi
- --       ON p.product_id = oi.product_id
+  FROM products p
+  RIGHT JOIN order_items oi
+        ON p.product_id = oi.product_id
 
 
 
